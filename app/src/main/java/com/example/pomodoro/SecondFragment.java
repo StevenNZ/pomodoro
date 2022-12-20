d61086c6ebe55998974d2b47519268fba89268f2
StevenNZ <listevennz@gmail.com>
2022-12-20 22:51:24 +1300

    feat: timer functionality has been implemented and correctly works

--- app/src/main/java/com/example/pomodoro/SecondFragment.java
+++ app/src/main/java/com/example/pomodoro/SecondFragment.java
@@ -1,9 +1,12 @@
 package com.example.pomodoro;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,8 +14,16 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.pomodoro.databinding.FragmentSecondBinding;
 
+import java.util.Locale;
+
 public class SecondFragment extends Fragment {
 
+    private static long initialTime = 600000;
+
+    private boolean isRunning = false;
+    private long remainingTime = initialTime;
+
+    private CountDownTimer timer;
     private FragmentSecondBinding binding;
 
     @Override
@@ -29,6 +40,20 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("clicky");
+                if (isRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        updateTimer();
+
         binding.buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +61,36 @@ public class SecondFragment extends Fragment {
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+    }
+
+    private void updateTimer() {
+        int minutes = (int) (remainingTime/1000) / 60;
+        int seconds = (int) (remainingTime/1000) % 60;
+
+        String remainingTimeText = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        binding.textTimer.setText(remainingTimeText);
+    }
+
+    private void startTimer() {
+        timer = new CountDownTimer(remainingTime, 1000) {
+            @Override
+            public void onTick(long timeUntilFinish) {
+                remainingTime = timeUntilFinish;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                // TODO: 20/12/22
+            }
+        }.start();
+
+        isRunning = true;
+    }
+
+    private void pauseTimer() {
+        timer.cancel();
+        isRunning = false;
     }
 
     @Override
