5172bad673946843b09fbc19bf31f8389a65d54a
StevenNZ <listevennz@gmail.com>
2023-01-09 18:28:03 +1300

    feat: Updated the navigation path of the app by incorporating the custom setting fragment

--- app/src/main/java/com/example/pomodoro/CustomSetting.java
+++ app/src/main/java/com/example/pomodoro/CustomSetting.java
@@ -2,14 +2,15 @@ package com.example.pomodoro;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.pomodoro.databinding.CustomSettingBinding;
-import com.example.pomodoro.databinding.PomodoroTimerBinding;
 
 
 public class CustomSetting extends Fragment {
@@ -21,5 +22,24 @@ public class CustomSetting extends Fragment {
                              Bundle savedInstanceState) {
         binding = CustomSettingBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(CustomSetting.this)
+                        .navigate(R.id.action_customSetting_to_pomodoro);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
