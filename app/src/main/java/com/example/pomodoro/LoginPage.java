81d01148b2956ea9d827f87eb09c4ee575d8276d
StevenNZ <listevennz@gmail.com>
2023-02-08 00:06:00 +1300

    feat: added function to update user account when initially starting the app

--- app/src/main/java/com/example/pomodoro/LoginPage.java
+++ app/src/main/java/com/example/pomodoro/LoginPage.java
@@ -1,7 +1,6 @@
 package com.example.pomodoro;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,11 +24,26 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginPage extends Fragment {
 
+    protected static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pomodoro-2bd96-default-rtdb.firebaseio.com/");
+
     private LoginPageBinding binding;
 
-    private FirebaseAuth auth;
+    private static FirebaseAuth auth = FirebaseAuth.getInstance();
 
-    protected static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://pomodoro-2bd96-default-rtdb.firebaseio.com/");
+    public static void updateUserAccount() {
+        String uid = auth.getUid();
+        databaseReference.child("Users").child(uid).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                DataSnapshot snapshot = task.getResult();
+
+                UserAccount.setUID(uid);
+                retrieveUserInfo(snapshot);
+                retrieveUserStats(snapshot);
+                retrieveUserCustom(snapshot);
+            }
+        });
+    }
 
     @Override
     public View onCreateView(
@@ -43,8 +57,6 @@ public class LoginPage extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        auth = FirebaseAuth.getInstance();
 
         binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,17 +111,19 @@ public class LoginPage extends Fragment {
                     });
                 } else {
                     Toast.makeText(requireContext(), "Please verify your email", Toast.LENGTH_SHORT).show();
+                    auth.signOut();
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception exception) {
                 Toast.makeText(requireContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                auth.signOut();
             }
         });
     }
 
-    private void retrieveUserCustom(DataSnapshot dataSnapshot) {
+    private static void retrieveUserCustom(DataSnapshot dataSnapshot) {
         DataSnapshot customSnapshot = dataSnapshot.child("Custom");
 
         String titleOne = (String) customSnapshot.child("Title One").getValue();
@@ -131,7 +145,7 @@ public class LoginPage extends Fragment {
         UserAccount.setCustomLongTwo(longTwo);
     }
 
-    private void retrieveUserStats(DataSnapshot dataSnapshot) {
+    private static void retrieveUserStats(DataSnapshot dataSnapshot) {
         DataSnapshot statsSnapshot = dataSnapshot.child("Statistics");
 
         int pomodoroTotal = Integer.parseInt(String.valueOf(statsSnapshot.child("Pomodoro Total").getValue()));
@@ -145,7 +159,7 @@ public class LoginPage extends Fragment {
         UserAccount.setPomodoroCycles(cycleTotal);
     }
 
-    private void retrieveUserInfo(DataSnapshot dataSnapshot) {
+    private static void retrieveUserInfo(DataSnapshot dataSnapshot) {
         UserAccount.setEmailAddress(String.valueOf(dataSnapshot.child("Email Address").getValue()));
         UserAccount.setUsername(String.valueOf(dataSnapshot.child("Username").getValue()));
         UserAccount.setPassword(String.valueOf(dataSnapshot.child("Password").getValue()));
