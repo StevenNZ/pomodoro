688a8eb444b57a1e55609264b7dc45a4f310c100
StevenNZ <listevennz@gmail.com>
2023-02-07 20:57:29 +1300

    feat: implementation for forgot password

---
+++ app/src/main/java/com/example/pomodoro/ForgotPassword.java
@@ -0,0 +1,57 @@
+package com.example.pomodoro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.pomodoro.databinding.ForgotPasswordBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends Fragment {
+
+    ForgotPasswordBinding binding;
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = ForgotPasswordBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.emailRecoverText.getText().toString();
+
+                if (email.isEmpty()) {
+                    Toast.makeText(requireContext(), "Make sure to enter your email address", Toast.LENGTH_LONG).show();
+                } else {
+                    auth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(requireContext(), "An email has been successfully sent", Toast.LENGTH_LONG).show();
+
+                            requireActivity().onBackPressed();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            Toast.makeText(requireContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
