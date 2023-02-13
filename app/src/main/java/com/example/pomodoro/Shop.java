e92024b43c85a203362b76dd8e9224a46d848080
StevenNZ <listevennz@gmail.com>
2023-02-13 17:11:37 +1300

    feat: initial commit

---
+++ app/src/main/java/com/example/pomodoro/Shop.java
@@ -0,0 +1,32 @@
+package com.example.pomodoro;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pomodoro.databinding.FragmentShopBinding;
+
+
+public class Shop extends Fragment {
+
+    FragmentShopBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentShopBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
